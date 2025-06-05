c9a03d305c82b95e402d92842c335bf81aa9c1ea
RenatoBS24 <pro51.ballena@gmail.com>
2025-06-05 02:16:23 -0500

    creacion de vista para los libros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './components/login/login';
 import {Dashboard} from './components/dashboard/dashboard';
 import {Usuario} from './components/usuario/usuario';
+import {BookComponent} from './components/book/book';
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'panel', component: Dashboard, children:[
-      {path: 'users', component: Usuario}
+      {path: 'users', component: Usuario},
+      {path:'books',component:BookComponent}
     ]}
 ];
 
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {RouterLink} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../../services/auth/auth-service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,7 @@ import {HttpClient} from '@angular/common/http';
 })
 export class LoginComponent {
   loginForm: FormGroup;
-  constructor(private fb:FormBuilder,private http:HttpClient) {
+  constructor(private fb:FormBuilder,private authService:AuthService,private http:HttpClient, private router:Router) {
     this.loginForm = this.fb.group({
       email:[''],
       password:['']
@@ -29,7 +30,8 @@ export class LoginComponent {
       .subscribe({
         next: (response:any) =>{
           console.log(response);
-          sessionStorage.setItem('jwt',response)
+          this.authService.setToken(response.token);
+          this.router.navigate(['/panel']);
         },
         error: () => {
           this.errorMessage = 'Credenciales incorrectas'
